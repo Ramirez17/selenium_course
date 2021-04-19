58139da7b73064240f4602b3bf46c21fabbfed71
tester <111>
2021-04-19 08:42:53 +0300

    init

--- test_abs_project.py
+++ test_abs_project.py
@@ -12,8 +12,7 @@ class TestAbs(unittest.TestCase):
     def test_abs1(self):
         self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
 
-#тест из задачи 1.6 шаг 2
-class TestPage(unittest.TestCase):
+    # 1 тест
     def test_page1(self):
         hostname = "10.3.1.2"
         port = "8080"
@@ -34,23 +33,84 @@ class TestPage(unittest.TestCase):
         def open_a_page(driver, url):
             driver.get(url)
 
-        Thread(target=open_a_page, args=(driver, "http://suninjuly.github.io/find_xpath_form")).start()
+        Thread(target=open_a_page, args=(driver, "http://suninjuly.github.io/registration1.html")).start()
         Thread(target=enter_proxy_auth, args=(proxy_username, proxy_password)).start()
 
         time.sleep(3)
 
-        input1 = driver.find_element_by_tag_name("input")
+        input1 = driver.find_element_by_xpath("//div[@class='first_block']/div/input[@class='form-control first']")
         input1.send_keys("Ivan")
-        input2 = driver.find_element_by_name("last_name")
+        input2 = driver.find_element_by_xpath("//div[@class='first_block']/div/input[@class='form-control second']")
         input2.send_keys("Petrov")
-        input3 = driver.find_element_by_class_name("city")
-        input3.send_keys("Smolensk")
-        input4 = driver.find_element_by_id("country")
-        input4.send_keys("Russia")
-
-        button = driver.find_element_by_xpath('//button[contains(text(), "Submit")]')
+        input3 = driver.find_element_by_xpath(
+            "//div[@class='first_block']/div[@class='form-group third_class']/input[@class='form-control third']")
+        input3.send_keys("example@email.com")
+        time.sleep(1)
+        button = driver.find_element_by_css_selector("button.btn")
         button.click()
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+        # находим элемент, содержащий текст
+        welcome_text_elt = driver.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
 
+        time.sleep(1)
+        # закрываем браузер после всех манипуляций
+        driver.quit()
+
+    # 2ой тест: задача 1.6 шаг 2
+
+
+    def test_page2(self):
+        hostname = "10.3.1.2"
+        port = "8080"
+        proxy_username = "ivaha_ay"
+        proxy_password = "aJb6YkaD"
+
+        chrome_options = Options()
+        chrome_options.add_argument('--proxy-server={}'.format(hostname + ":" + port))
+        driver = webdriver.Chrome(options=chrome_options)
+
+        def enter_proxy_auth(proxy_username, proxy_password):
+            time.sleep(1)
+            pyautogui.typewrite(proxy_username)
+            pyautogui.press('tab')
+            pyautogui.typewrite(proxy_password)
+            pyautogui.press('enter')
+
+        def open_a_page(driver, url):
+            driver.get(url)
+
+        Thread(target=open_a_page, args=(driver, "http://suninjuly.github.io/registration2.html")).start()
+        Thread(target=enter_proxy_auth, args=(proxy_username, proxy_password)).start()
+
+        input1 = driver.find_element_by_xpath("//div[@class='first_block']/div/input[@class='form-control first']")
+        input1.send_keys("Ivan")
+        input2 = driver.find_element_by_xpath("//div[@class='first_block']/div/input[@class='form-control second']")
+        input2.send_keys("Petrov")
+        input3 = driver.find_element_by_xpath(
+            "//div[@class='first_block']/div[@class='form-group third_class']/input[@class='form-control third']")
+        input3.send_keys("example@email.com")
+        time.sleep(1)
+        button = driver.find_element_by_css_selector("button.btn")
+        button.click()
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+        # находим элемент, содержащий текст
+        welcome_text_elt = driver.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+
+        time.sleep(1)
+        # закрываем браузер после всех манипуляций
+        driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
