57785b5a20ca07faab24d3eeb24c6595dcaa83f0
tester <111>
2021-04-21 09:50:40 +0300

    2 теста на demoqa.com

--- test_shop.py
+++ test_shop.py
@@ -37,14 +37,46 @@ class TestAbs(unittest.TestCase):
 
         driver.implicitly_wait(5)
         fullNameValue = "Andrew"
-        fullNameValue1 = "Andrew1"
         fullName = driver.find_element_by_xpath("//input[@id='userName']")
         fullName.send_keys(fullNameValue)
 
         driver.find_element_by_xpath("//button[@id='submit']").click()
 
-        nameResult = driver.find_element_by_xpath("//p[@id='name']").text
-        assert fullNameValue1 == nameResult, "Имя введено некорректно"
+        fullNameResult = driver.find_element_by_xpath("//p[@id='name']").text
+        print(fullNameResult)
+        time.sleep(1)
+        # закрываем браузер после всех манипуляций
+        driver.quit()
+
+    def test_doubleClick(self):
+        hostname = "10.3.1.2"
+        port = "8080"
+        proxy_username = "ivaha_ay"
+        proxy_password = "aJb6YkaD"
+
+        chrome_options = Options()
+        chrome_options.add_argument('--proxy-server={}'.format(hostname + ":" + port))
+        driver = webdriver.Chrome(options=chrome_options)
+
+        def enter_proxy_auth(proxy_username, proxy_password):
+            time.sleep(1)
+            pyautogui.typewrite(proxy_username)
+            pyautogui.press('tab')
+            pyautogui.typewrite(proxy_password)
+            pyautogui.press('enter')
+
+        def open_a_page(driver, url):
+            driver.get(url)
+
+        Thread(target=open_a_page, args=(driver, "https://demoqa.com/buttons")).start()
+        Thread(target=enter_proxy_auth, args=(proxy_username, proxy_password)).start()
+
+        driver.implicitly_wait(5)
+
+        waitResult = "You have done a double click"
+        doubleButton = driver.find_element_by_xpath("//button[@id='doubleClickBtn']")
+        webdriver.ActionChains(driver).double_click(doubleButton).perform()
+        doubleResult = driver.find_element_by_xpath("//p[@id='doubleClickMessage']").text
 
         time.sleep(1)
         # закрываем браузер после всех манипуляций
@@ -52,4 +84,3 @@ class TestAbs(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
